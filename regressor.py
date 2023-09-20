cda9d8f23e19862495f1ca297414f93abad6f1f1
younghwan-cho-dev <yc71@illinois.edu>
2023-09-20 12:27:11 -0500

    Numerical issue resolved. pysindy replicated

--- regressor.py
+++ regressor.py
@@ -21,42 +21,40 @@ def STLS_algo(X, y, treshold, n_iter):
 
 def soft_threshold(rho,lamda):
     '''Soft threshold function used for lasso regression'''
-    if rho < -lamda:
-        return (rho + lamda)
-    elif rho >  lamda:
-        return (rho - lamda)
+    if rho < -lamda/2:
+        return (rho + lamda/2)
+    elif rho >  lamda/2:
+        return (rho - lamda/2)
     else: 
         return 0
     
-def coordinate_descent_lasso(theta, X, y, reg_term=1, num_iters=1000, include_bias = False):
+def coordinate_descent_lasso(theta, X, y, reg_term=1, num_iters=100, include_bias = False):
     '''Coordinate gradient descent for lasso regression - for unnormalized data. '''
     
     #Initialisation of useful values 
     N, M = X.shape
 
-    Z = np.linalg.norm(X, axis = 0) 
-
-    print(np.linalg.norm(X, axis = 0))
+    Z = np.sum(X**2,axis=0)
 
     #Looping until max number of iterations
     for i in range(num_iters): 
-        #Looping through each coordinate
+        #Looping through each independent variable
         for j in range(M):
-            
+            Z_j = Z[j]
             #Vectorized implementation
             y_pred = X @ theta
-            rho = X[:,j].T @ (y - y_pred  + theta[j]*X[:,j])
-            
+            rho = (X[:,j].reshape(-1,1).T @ (y - y_pred  + theta[j]*X[:,j].reshape(-1,1)))[0][0] # 2dim np.array's value
+
             #Checking intercept parameter
             if include_bias == True:  
                 if j == 0: 
                     theta[j,:] =  rho 
                 else:
-                    theta[j,:] =  soft_threshold(rho, reg_term)  
+                    theta[j,:] =  soft_threshold(rho, reg_term) / Z_j 
 
             if include_bias == False:
 
-                theta[j,:] =  soft_threshold(rho, reg_term)   
+                theta[j,:] =  soft_threshold(rho, reg_term) / Z_j
             
     return theta.flatten()
 
